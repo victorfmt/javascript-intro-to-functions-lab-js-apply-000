2f4c003a330d5d6008925ed62a8d77c46e828c3e
Victor Torres <victor.fmt@gmail.com>
2019-09-10 19:31:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,17 +22,3 @@ function sayHiToGrandma(string){
   return "YES INDEED!";
 }
 
-
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
